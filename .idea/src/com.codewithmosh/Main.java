c128f01793f6788525f1a94c8c19c45e03993bba
Esmail Ahmed <113549037+esmaaaill@users.noreply.github.com>
2026-09-21 08:13:17 +0300

    First Java Commit to start the new Repo

--- .idea/src/com.codewithmosh/Main.java
+++ .idea/src/com.codewithmosh/Main.java
@@ -2,6 +2,6 @@ package com.codewithmosh;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Let's Start My Java Journey!");
+        System.out.println("Let's Start The Java Bootcamp Journey!");
     }
 }
