5d4bd50e0ba4991a760b8d601dd89c47f0161328
Esmail Ahmed <113549037+esmaaaill@users.noreply.github.com>
2026-09-21 08:27:12 +0300

    Intial Push

--- .idea/src/com.codewithmosh/Main.java
+++ .idea/src/com.codewithmosh/Main.java
@@ -2,6 +2,7 @@ package com.codewithmosh;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Let's Start The Java Bootcamp Journey!");
+        System.out.println("Let's Start The Java Bootcamp Journey!!");
     }
-}
+
+};
